bda61b702c2a4dc35f14c7ec50eec74f35345317
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-08-21 20:57:23 +0545

    chore: remove unused code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,7 @@
-import { useState } from 'react';
-
-import { Greet } from '../wailsjs/go/main/App';
-import './App.css';
-import logo from './assets/images/logo-universal.png';
-
 function App() {
-  const [resultText, setResultText] = useState(
-    'Please enter your name below 👇'
-  );
-  const [name, setName] = useState('');
-  const updateName = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setName(e.target.value);
-  const updateResultText = (result: string) => setResultText(result);
-
-  function greet() {
-    Greet(name).then(updateResultText);
-  }
-
   return (
-    <div id="App">
-      <img src={logo} id="logo" alt="logo" />
-      <div id="result" className="result">
-        {resultText}
-      </div>
-      <div id="input" className="input-box">
-        <input
-          id="name"
-          className="input"
-          onChange={updateName}
-          autoComplete="off"
-          name="input"
-          type="text"
-        />
-        <button className="btn" onClick={greet}>
-          Greet
-        </button>
-      </div>
+    <div className="bg-gray-900 min-h-screen text-slate-100 flex justify-center items-center text-9xl">
+      <p>Hello World!</p>
     </div>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import './style.css';
+import './index.css';
 
 const container = document.getElementById('root');
 
